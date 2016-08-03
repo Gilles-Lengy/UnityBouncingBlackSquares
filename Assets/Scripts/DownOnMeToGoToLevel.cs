9ca11a34a1b3866fa7d1dada7c1ba09ddf855f85
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-03 16:34:54 +0200

    Remove the useless script for a text that doesn't exixts anymore, Add a script to go to a chosen scene onmousedown on it

---
+++ Assets/Scripts/DownOnMeToGoToLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DownOnMeToGoToLevel : MonoBehaviour
+{
+   public string level = "Level_1";
+
+    void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown on me");
+        Time.timeScale = 1F;// http://docs.unity3d.com/ScriptReference/Time-timeScale.html
+        SceneManager.LoadScene(level);
+        Debug.Log("On Mouse Down click anywhere");
+    }
+}
