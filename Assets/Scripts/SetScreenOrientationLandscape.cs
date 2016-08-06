86d8244efd2fde86671d659a846af09a0393627e
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-06 16:33:09 +0200

    Landscape autorotation only

---
+++ Assets/Scripts/SetScreenOrientationLandscape.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetScreenOrientationLandscape : MonoBehaviour {
+
+    void Start()
+    {
+        Screen.autorotateToPortrait = false;
+
+        Screen.autorotateToPortraitUpsideDown = false;
+
+        Screen.autorotateToLandscapeLeft = true;
+
+        Screen.autorotateToLandscapeRight = true;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+}
