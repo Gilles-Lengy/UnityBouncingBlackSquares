3e66fff7155ee9293f4df9a4e8bf63ad3baa58d5
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-08 22:06:47 +0200

    Button play in the MainMenu scene

---
+++ Assets/Scripts/OnClickGoToLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OnClickGoToLevel : MonoBehaviour {
+
+    public void GoToLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+        Debug.Log("Go to level " + level + " from OnClick ");
+    }
+}
