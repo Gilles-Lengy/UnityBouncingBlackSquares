595f0ca7343bc4b2830bf40de5fa4abe37ce5b37
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-08 09:01:45 +0200

    Display a high score wich is always 0 for now

---
+++ Assets/Scripts/DisplayHighScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayHighScore : MonoBehaviour {
+
+    private Text highScoreText;
+    public int highScore;
+
+    // Use this for initialization
+    void Start () {
+        highScore = PlayerPrefs.GetInt("highscore", highScore);
+        highScoreText.text = highScore.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
