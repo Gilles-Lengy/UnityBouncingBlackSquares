2e9aac62444075f2092b3c25b7bf5e6ef04dd058
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-10 10:47:32 +0200

    Class WaitForSecondsTextChangeColor rewritten

--- Assets/Scripts/WaitForSecondsTextChangeColor.cs
+++ Assets/Scripts/WaitForSecondsTextChangeColor.cs
@@ -18,18 +18,35 @@ public class WaitForSecondsTextChangeColor : MonoBehaviour {
     public byte displayedTextColorG1 = 0;
     public byte displayedTextColorB1 = 0;
     public byte displayedTextColorA1 = 255;
-
+    /*
     void Start()
     {
         displayedText.color = new Color32(displayedTextColorR0, displayedTextColorG0, displayedTextColorB0, displayedTextColorA0);
-        StartCoroutine(Example());
+        StartCoroutine(ChangeColor(waitDuration));
     }
 
-    IEnumerator Example()
+    IEnumerator ChangeColor(float waitTime)
     {
         print(Time.time);
-        yield return new WaitForSeconds(waitDuration);
+        yield return new WaitForSeconds(waitTime);
         displayedText.color = new Color32(displayedTextColorR1, displayedTextColorG1, displayedTextColorB1, displayedTextColorA1);
         print(Time.time);
+    }
+    */
+
+    IEnumerator ChangeColor()
+    {
+        // suspend execution for "waitDuration" seconds
+        yield return new WaitForSeconds(waitDuration);
+        displayedText.color = new Color32(displayedTextColorR1, displayedTextColorG1, displayedTextColorB1, displayedTextColorA1);
+        print("WaitAndPrint " + Time.time);
+    }
+    IEnumerator Start()
+    {
+        print("Starting " + Time.time);
+        displayedText.color = new Color32(displayedTextColorR0, displayedTextColorG0, displayedTextColorB0, displayedTextColorA0);
+        // Start function WaitAndPrint as a coroutine
+        yield return StartCoroutine("ChangeColor");
+        print("Done " + Time.time);
     }
 }
