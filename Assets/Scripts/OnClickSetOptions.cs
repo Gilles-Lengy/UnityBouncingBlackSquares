786245dfea1b4f3102837f3a4f2c9deb0b8bc34c
Gilles Lengy <gilles.lengy@gmail.com>
2016-08-14 14:50:13 +0200

    Options setting sound and visual fx on or off works

--- Assets/Scripts/OnClickSetOptions.cs
+++ Assets/Scripts/OnClickSetOptions.cs
@@ -1,28 +1,34 @@
-﻿using UnityEngine;using UnityEngine.SceneManagement;
-public class OnClickSetOptions : MonoBehaviour {
+﻿using UnityEngine;using UnityEngine.SceneManagement;using UnityEngine.UI;public class OnClickSetOptions : MonoBehaviour {    private Text textSoundOnOff;
+    private Text textVisualFxOnOff;
 
-    public void SetOption(string option)
+    private string playerPrefsSoundOnOff;
+    private string playerPrefsVisualFXOnOff;
+
+    // Use this for initialization
+    void Start()
     {
-
-        switch (option)
-        {
-
-            case "VisualFX":
-                SceneManager.LoadScene("Level_1");
-                break;
-            case "SoundOnOff":
-                SceneManager.LoadScene("Level_1");
-                break;
-            default:
-                GoToScene("Options");
-                break;
-        }
-    }
-
-    public void GoToScene(string SceneToGoTo)
-    {
-        Debug.Log("SceneToGoTo " + SceneToGoTo + " from SetOptions ");
-        SceneManager.LoadScene(SceneToGoTo);
-
-    }
-}
+        Debug.Log("Start OnClickSetOptions");
+        textSoundOnOff = GameObject.Find("TextSoundOnOff").GetComponent<Text>();
+        textVisualFxOnOff = GameObject.Find("TextVisualFxOnOff").GetComponent<Text>();
+        playerPrefsSoundOnOff = PlayerPrefs.GetString("soundOnOff","ON");
+        playerPrefsVisualFXOnOff = PlayerPrefs.GetString("visualFxOnOff", "ON");
+        textSoundOnOff.text = playerPrefsSoundOnOff;
+        textVisualFxOnOff.text = playerPrefsVisualFXOnOff;
+    }    public void SetOption(string option)    {        switch (option)        {
+            case "Sound":
+                playerPrefsSoundOnOff = PlayerPrefs.GetString("soundOnOff", "ONF");                if (playerPrefsSoundOnOff == "OFF")
+                {
+                    textSoundOnOff.text = "ON";
+                    PlayerPrefs.SetString("soundOnOff", "ON");
+                }
+                else
+                {
+                    textSoundOnOff.text = "OFF";
+                    PlayerPrefs.SetString("soundOnOff", "OFF");
+                }                break;            case "VisualFX":                playerPrefsVisualFXOnOff = PlayerPrefs.GetString("visualFxOnOff", "ONF");                if (playerPrefsVisualFXOnOff == "OFF") {
+                    textVisualFxOnOff.text = "ON";
+                    PlayerPrefs.SetString("visualFxOnOff", "ON");
+                } else {
+                    textVisualFxOnOff.text = "OFF";
+                    PlayerPrefs.SetString("visualFxOnOff", "OFF");
+                }                break;            default:                GoToScene("Options");                break;        }    }    public void GoToScene(string SceneToGoTo)    {        Debug.Log("SceneToGoTo " + SceneToGoTo + " from SetOptions ");        SceneManager.LoadScene(SceneToGoTo);    }}
